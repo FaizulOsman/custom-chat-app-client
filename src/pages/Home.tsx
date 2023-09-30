96c6a8093133a8a45cda8c06705d533568d58761
FaizulOsman <faizulosmanrabby@gmail.com>
2023-09-30 20:59:10 +0600

    feat: home page ui changed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,30 +20,27 @@ export default function Home() {
   };
 
   return (
-    <div className="min-h-screen mt-10">
-      <h1 className="text-2xl font-semibold text-center text-blue-700">
-        All Users
-      </h1>
-      <div className="text-center flex gap-2 items-center justify-center my-5">
+    <div className="min-h-screen pt-10 bg-[#050816]">
+      <div className="text-center flex gap-2 items-center justify-center mb-5 px-4">
         <input
           type="text"
           placeholder="Search UserName or Email"
           onChange={(e) => handleFindUser(e)}
-          className="input input-bordered border-blue-500 w-full max-w-xs"
+          className="input input-bordered border-blue-500 bg-[#151030] text-white w-full max-w-xs"
         />
       </div>
-      <div className="w-11/12 sm:w-8/12 mx-auto">
+      <div className="w-11/12 sm:w-8/12 mx-auto min-h-[70vh]">
         {allUsersExpectMe?.map((user: any) => (
           <Link
             to={myProfile ? `chat?email=${user?.email}` : `/login`}
             key={user?.id}
           >
-            <div className="flex justify-between hover:bg-slate-100 items-center mb-2 shadow-md mt-4 rounded-md p-4 cursor-pointer">
+            <div className="flex justify-between bg-[#151030] text-white hover:bg-[#1a1345] items-center mb-2 shadow-md mt-4 rounded-md p-4 cursor-pointer">
               <div className="flex items-center">
                 <img
                   src="https://static.vecteezy.com/system/resources/previews/009/383/461/original/man-face-clipart-design-illustration-free-png.png"
                   alt={user?.name}
-                  className="w-10 h-10 rounded-full mr-3"
+                  className="w-10 h-10 rounded-full mr-3 border p-[2px]"
                 />
                 <div>
                   <p className="font-semibold">{user?.name}</p>
